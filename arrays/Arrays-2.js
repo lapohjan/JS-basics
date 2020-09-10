293553b0901e92b48637993bf6204f13347ca06e
lapohjan <59822237+lapohjan@users.noreply.github.com>
2020-09-10 09:56:32 +0300

    add files for array tasks

---
+++ arrays/Arrays-2.js
@@ -0,0 +1,9 @@
+
+let numbers = [];
+
+
+
+
+// 2.	Make a program that draws 7 digits for Lotto numbers. You have to make sure that same number isn’t there many times.
+// You can use array that has 40 pieces of zeros. When you get a random number that is the index of the array and you make the value inside zero to one, but if it already was one you draw a new random number.
+// At the end you print out the indexes that has one as a value inside.
